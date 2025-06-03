7584ec6653431bb51d0df809fe73a02dbaf04691
Iuri Paiva <47199107+iuripaiva@users.noreply.github.com>
2025-06-03 18:24:10 -0300

    Update script.js birthday project

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ let age = document.getElementById("age");
 const body = document.querySelector("body");
 const toggle = document.getElementById("toggle-btn");
 const togglebkg = document.getElementById("toggle-bkg");
+const partyhat = document.getElementById("party-hat");
+const partyconfetti = document.getElementById("party-confetti");
+let isBirthday = false;
 
 // Theme switch
 toggle.onclick = function(){
@@ -10,21 +13,23 @@ toggle.onclick = function(){
     body.classList.toggle("active");
 }
 
-togglebkg.onclick = function(){
-    toggle.classList.toggle("active");
-    togglebkg.classList.toggle("active");
-    body.classList.toggle("active");
-}
-
 // Script to update my age based on my birthday
 let today = new Date();
 let day = String(today.getDate()).padStart(2, '0');
-let month = String(today.getMonth() + 1).padStart(2, '0'); //0 = Jan
+let month = String(today.getMonth() + 1).padStart(2, '0'); // 0 = Jan
 let year = today.getFullYear();
 let newAge = year - 1997;
 
 if ((month == "06" && day == "27") || (month > "06")){
     age.innerHTML = newAge;
+} else if (month == "06" && day == "27") {
+    age.innerHTML = newAge;
+    isBirthday = true;
 } else {
     age.innerHTML = newAge - 1;
+}
+
+if (isBirthday == true){
+    partyhat.classList.toggle("active");
+    partyconfetti.classList.toggle("active");
 }
